926caf271ef7a612f5043dcbbc739083dc5b5208
nilocy <mahaffec@gmail.com>
2017-02-12 14:29:25 -0500

    changes mouseover to hover for mobile

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,26 +1,29 @@
+var cardFlip = function(){
+
+      if ( $('.flipped').length > 0 ){
+        $('.flipped').removeClass('flipped');
+        $('.radicalize.description').css('display','none');
+        $('.soundtrak.description').css('display','none');
+      }else{
+        $('.card').addClass('flipped');
+      }
+
+  return false;
+}
+
 $(function(){
-  $('.flip').mouseenter(function(){
 
-        if ( $('.flipped').length > 0 ){
-          $('.flipped').removeClass('flipped');
-          $('.radicalize.description').css('display','none');
-          $('.soundtrak.description').css('display','none');
-        }else{
-          $('.card').addClass('flipped')
-        }
-
-    return false;
-  });
+  $('.flip').hover(cardFlip);
 
   $('.radicalize').hover(function(){
-    if( $('.soundtrak.description').css('display') == 'block' )
+    if( $('.soundtrak.description').css('display') == 'block' );
       $('.soundtrak.description').css('display','none');
     //check if back showing
     if( $('.flipped').length > 0 )
       $('.radicalize.description').css('display','block');
   });
   $('.soundtrak').hover(function(){
-    if( $('.radicalize.description').css('display') == 'block' )
+    if( $('.radicalize.description').css('display') == 'block' );
       $('.radicalize.description').css('display','none');
       //check if back showing
     if( $('.flipped').length > 0 )
