0c63ebaa7abc51251e2f46e87365ca327f0c5378
nilocy <mahaffec@gmail.com>
2017-02-12 13:58:56 -0500

    adds project descriptions

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,12 +1,30 @@
 $(function(){
   $('.flip').mouseenter(function(){
 
-        if ( $('.flipped').length > 0 )
+        if ( $('.flipped').length > 0 ){
           $('.flipped').removeClass('flipped');
-        else
+          $('.radicalize.description').css('display','none');
+          $('.soundtrak.description').css('display','none');
+        }else{
           $('.card').addClass('flipped')
+        }
 
     return false;
+  });
+
+  $('.radicalize').hover(function(){
+    if( $('.soundtrak.description').css('display') == 'block' )
+      $('.soundtrak.description').css('display','none');
+    //check if back showing
+    if( $('.flipped').length > 0 )
+      $('.radicalize.description').css('display','block');
+  });
+  $('.soundtrak').hover(function(){
+    if( $('.radicalize.description').css('display') == 'block' )
+      $('.radicalize.description').css('display','none');
+      //check if back showing
+    if( $('.flipped').length > 0 )
+      $('.soundtrak.description').css('display','block');
   });
 
 });
